98ebe019028d6431e3ed13e8178541bb83da493b
vlargius <vlargius@gmail.com>
2019-01-31 22:42:27 +0300

    fix project settings

---
+++ neurolife/neurolife/actor_constant.cpp
@@ -0,0 +1,8 @@
+#include "actor_constant.h"
+
+int deafault_actor::decay;
+double deafault_actor::max_velocity;
+double deafault_actor::max_acceleration;
+int deafault_actor::hp;
+double deafault_actor::meal_up;
+double deafault_actor::eps;
